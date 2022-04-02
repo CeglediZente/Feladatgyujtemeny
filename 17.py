df5f721a8afabfe7af6d34aa0b6bccbcb03ddc0c
Ceglédi Zente Holló <nadizente@gmail.com>
2022-04-02 14:06:42 +0200

    17

---
+++ 17.py
@@ -0,0 +1,8 @@
+""
+Az alábbi program célja, hogy egy háromszög két
+belső szögéből kiszámolja a harmadik belső szöget.
+"""
+szog1 = 43
+szog2 = 52.19
+
+print("A harmadik belső szög összege: 180-szog1-szog2")
