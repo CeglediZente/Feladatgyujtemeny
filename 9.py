22c73e794652b64370746f44b5c1ef08d824a675
Ceglédi Zente Holló <nadizente@gmail.com>
2022-04-02 14:05:41 +0200

    17

--- 9.py
+++ 9.py
@@ -1,4 +1,8 @@
-szam  12
-szo = strszam)
-gyumolcs = 'szilva
-print("szam", gyumolcs)
+""
+Az alábbi program célja, hogy egy háromszög két
+belső szögéből kiszámolja a harmadik belső szöget.
+"""
+szog1 = 43
+szog2 = 52.19
+
+print("A harmadik belső szög összege: 180-szog1-szog2")
